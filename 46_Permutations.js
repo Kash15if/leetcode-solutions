d786bddcf4f5520f800c81cb49cd4ce9cbe800e2
Kash15if <kash15if@yahoo.com>
2022-09-22 09:29:47 +0530

    Added permutation using swap value without selectedArray

--- 46_Permutations.js
+++ 46_Permutations.js
@@ -34,3 +34,38 @@ function getPermutation(nums, selected, arr, out, n) {
 
   return;
 }
+
+// Using swap and  pivot left permuttion calculated , right to be calc
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let out = [],
+    n = nums.length;
+
+  getPermutation(nums, 0, out, n);
+
+  return out;
+};
+
+function getPermutation(nums, pivot, out, n) {
+  if (pivot >= n) {
+    out.push(nums);
+    return;
+  }
+
+  for (let i = pivot; i < n; i++) {
+    swap(nums, i, pivot);
+    getPermutation([...nums], pivot + 1, out, n);
+    swap(nums, pivot, i);
+  }
+
+  return;
+}
+
+function swap(arr, l, r) {
+  let temp = arr[l];
+  arr[l] = arr[r];
+  arr[r] = temp;
+}
